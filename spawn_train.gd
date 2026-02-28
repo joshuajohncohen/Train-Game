353f658b76effc836d1b8734d1f7f04bc3abf8e5
izie <238573415+izieeee@users.noreply.github.com>
2026-02-28 14:24:53 -0500

    example commit message

--- spawn_train.gd
+++ spawn_train.gd
@@ -1,13 +1,24 @@
 extends Node3D
 
+@export var block_pkg: PackedScene # Drag your block.tscn here
+@export var spawn_interval: float = 2.0
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+@onready var spawn_point = $Spawnpoint_train
+@onready var death_point = $Deathpoint_train
 
-func spawn():
-	
+func _ready():
+	# Set up and start the timer
+	var timer = Timer.new()
+	add_child(timer)
+	timer.wait_time = spawn_interval
+	timer.timeout.connect(_on_timer_timeout)
+	timer.start()
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _on_timer_timeout():
+	if block_pkg:
+		var new_block = block_pkg.instantiate()
+		get_parent().add_child(new_block) # Add to world, not the spawner
+		
+		# Set initial position and give it the destination
+		new_block.global_position = spawn_point.global_position
+		new_block.target_position = death_point.global_position
