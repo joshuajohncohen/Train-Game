2958e0a4fe1bada71ea71eb6cfcbe6d9949cadd1
izie <238573415+izieeee@users.noreply.github.com>
2026-02-28 13:24:12 -0500

    No jump mid-air

--- move_player.gd
+++ move_player.gd
@@ -1,7 +1,7 @@
 extends CharacterBody3D
 
-const SPEED = 20
-const JUMP_VELOCITY = 20
+const SPEED = 10
+const JUMP_VELOCITY = 10
 const SENSITIVITY = .010
 #Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
@@ -23,7 +23,7 @@ func _physics_process(delta):
 		velocity.y -= gravity * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("move_jump"):
+	if Input.is_action_just_pressed("move_jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
