3498b1b7baf2989a6fb609c224175c42db9bfeb0
izie <238573415+izieeee@users.noreply.github.com>
2026-02-28 17:55:13 -0500

    Update change_points_text.gd

--- change_points_text.gd
+++ change_points_text.gd
@@ -1,11 +1,9 @@
 extends RichTextLabel
+@onready var points = 0
 
 func _ready():
 	# Enable BBCode programmatically if not done in inspector
 	bbcode_enabled = true
 	
 	# Setting text with bold and color tags
-	text = "This is [b]bold[/b] and this is [color=red]red[/color]."
-	
-	# Appending text without overwriting the whole buffer
-	append_text("\n[wave amp=50 freq=2]This text is waving![/wave]")
+	text = "Points: " + str(points)
