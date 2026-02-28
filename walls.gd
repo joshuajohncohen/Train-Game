c4a0b151b09dc7e45daf758416cc2d9787f6e2d4
izie <238573415+izieeee@users.noreply.github.com>
2026-02-28 18:30:36 -0500

    Added offset to floor

--- walls.gd
+++ walls.gd
@@ -9,4 +9,4 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	global_position.x = player.global_position.x
+	global_position.x = player.global_position.x + 22
