e81a6b5462d307922d61d71809b2dd9d0088d9f9
izie <238573415+izieeee@users.noreply.github.com>
2026-03-01 10:04:41 -0500

    Clean up points a bit

--- pointgiver.gd
+++ pointgiver.gd
@@ -1,13 +1,5 @@
 extends Node3D
 
-func POINTSYAY(body: Node3D) -> void:
-	print_debug("points yay triggered " + str(body))
-	
+func _on_area_3d_body_entered(body: Node3D) -> void:
 	if body is CharacterBody3D:
 		Global.player_score += 1
-
-
-
-func _on_area_3d_body_entered(body: Node3D) -> void:
-	POINTSYAY(body)
-	pass # Replace with function body.
