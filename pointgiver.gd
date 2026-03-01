7fc7fcfc8cc8c9d89d05b760c824d8e682313ec1
izie <238573415+izieeee@users.noreply.github.com>
2026-03-01 10:02:52 -0500

    Fixed points!!

--- pointgiver.gd
+++ pointgiver.gd
@@ -1,18 +1,13 @@
 extends Node3D
 
-func _body_entered(body: Node3D) -> void:
-	# 'body' is the object that just entered
-	print_debug("mrow")
-	print("aaaaa")
-	Global.player_score += 1
-	if body.is_in_group("Player"):
-		print("The Player has entered the zone!")
+func POINTSYAY(body: Node3D) -> void:
+	print_debug("points yay triggered " + str(body))
 	
-	# Or check for a specific name/type
 	if body is CharacterBody3D:
-		print("A character entered: ", body.name)
+		Global.player_score += 1
+
 
 
 func _on_area_3d_body_entered(body: Node3D) -> void:
-	_body_entered(body)
+	POINTSYAY(body)
 	pass # Replace with function body.
