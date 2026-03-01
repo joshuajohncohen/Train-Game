ef9fc636098d3ec0234d73f74639e06f25603390
Joshua Cohen <65085812+joshuajohncohen@users.noreply.github.com>
2026-03-01 11:32:38 -0500

    **First actually working** version of respawn/death
    Still TODO: Death on side of train as well as on the front (rn only the front of the train kills the player)

--- Player_Spawn.gd
+++ Player_Spawn.gd
@@ -1,14 +1,20 @@
 extends Node3D
 
 @onready var player = $Player_Spawn/Player
+@onready var playerC = $Player_Spawn/Player/CharacterBody3D
+@onready var head = $Player_Spawn/Player/CharacterBody3D/Head
+@onready var camera = $Player_Spawn/Player/CharacterBody3D/Head/Camera3D
 @onready var spawn = $Player_Spawn
 @onready var laneGen = $LaneGenerator
 
 func player_reset():
-	player.global_position = spawn.global_position
+	playerC.velocity = Vector3.ZERO
+	playerC.global_position = spawn.global_position
+	head.rotation = Vector3.ZERO
+	camera.rotation = Vector3.ZERO
 	Global.player_score = 0
 	if laneGen.has_method("reset_trains"):
 		laneGen.reset_trains()
 
 func _ready():
-	player.global_position = spawn.global_position
+	playerC.global_position = spawn.global_position
