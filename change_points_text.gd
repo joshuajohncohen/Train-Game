6f1c665a1792721a5f7c0062e81e824a62b11f56
izie <238573415+izieeee@users.noreply.github.com>
2026-03-01 10:38:24 -0500

    Points pretty

--- change_points_text.gd
+++ change_points_text.gd
@@ -3,6 +3,7 @@ extends RichTextLabel
 func _ready():
 	# Enable BBCode programmatically if not done in inspector
 	bbcode_enabled = true
+	add_theme_font_size_override("normal_font_size", 60)
 	
 func _process(_delta):
 	# Setting text with bold and color tags
