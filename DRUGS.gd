4c74f4812015f8715311b6126e93b1bed7cbb53a
izie <238573415+izieeee@users.noreply.github.com>
2026-03-01 15:23:02 -0500

    idk

--- DRUGS.gd
+++ DRUGS.gd
@@ -1,6 +1,5 @@
-extends PathFollow3D
+extends Node
 
-var speed: float
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -9,12 +8,4 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	update_speed()
-	loop_movement(delta)
-
-func update_speed():
-	speed = ((((Global.farthest_track - 10) / 10) ** 1.07) / 90) + 0.3
-
-func loop_movement(delta):
-	
-	progress_ratio += speed * delta
+	pass
