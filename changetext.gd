1c8b822844cf742a1393ce61641c3045745de91a
izie <238573415+izieeee@users.noreply.github.com>
2026-03-01 13:19:13 -0500

    nvm whyd i do that

--- changetext.gd
+++ changetext.gd
@@ -1,29 +1,23 @@
 extends Control
 
-@onready var text_objects = [$You, $Wouldnt, $Fare, $Evade]
-@onready var random_areas = [$RandomAreas/YouArea, $RandomAreas/WouldntArea, $RandomAreas/FareArea, $RandomAreas/EvadeArea]
+@onready var you = $You
+@onready var wouldnt = $Wouldnt
+@onready var fare = $Fare
+@onready var evade = $Evade
+@onready var text_objects = [you, wouldnt, fare, evade]
 
 func _ready():
-	for i in range(text_objects.size()):
-		var text_node = text_objects[i]
-		var area_node = random_areas[i]
-		
-		text_node.bbcode_enabled = true
-		set_font_size(text_node)
-		move_randomly_in_container(text_node, area_node)
-
-func set_font_size(node: RichTextLabel):
-	var size = randf_range(60, 73)
-	node.add_theme_font_size_override("normal_font_size", size)
-
-func move_randomly_in_container(node: Control, area: Control):
-	var area_rect = area.get_global_rect()
-	var max_x = area_rect.size.x - node.size.x
-	var max_y = area_rect.size.y - node.size.y
-	max_x = max(0, max_x)
-	max_y = max(0, max_y)
-
-	var random_x = randf_range(area_rect.position.x, area_rect.position.x + max_x)
-	var random_y = randf_range(area_rect.position.y, area_rect.position.y + max_y)
+	# Enable BBCode programmatically if not done in inspector
+	you.bbcode_enabled = true
+	wouldnt.bbcode_enabled = true
+	fare.bbcode_enabled = true
+	evade.bbcode_enabled = true
 	
-	node.global_position = Vector2(random_x, random_y)
+	for text_object in text_objects:
+		set_font_size(text_object)
+	
+func set_font_size(change_font_object):
+	var font_size_random = randf_range(60, 73)
+	change_font_object.add_theme_font_size_override("normal_font_size", font_size_random)
+	return font_size_random
+	
