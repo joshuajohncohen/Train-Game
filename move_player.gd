3261904672a755ff328ee12e11e88474d88cda6a
izie <238573415+izieeee@users.noreply.github.com>
2026-03-01 11:50:34 -0500

    clean comments

--- move_player.gd
+++ move_player.gd
@@ -13,6 +13,7 @@ func _ready():
 
 func _unhandled_input(event):
 	if event is InputEventMouseMotion:
+		head.rotation.y = clamp(head.rotation.y, -1, 1)
 		head.rotate_y(-event.relative.x * SENSITIVITY)
 		camera.rotate_x(-event.relative.y * SENSITIVITY)
 		camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-40), deg_to_rad(60))
@@ -32,11 +33,11 @@ func _physics_process(delta):
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir = Input.get_vector("move_left", "move_right", "move_back", "move_forward")
 
-#Calculate the forward and right directions based on the camera orientation.
+	#Calculate the forward and right directions based on the camera orientation.
 	var forward_dir = -camera.global_transform.basis.z
 	var right_dir = camera.global_transform.basis.x
 
-#Calculate the direction the player should move.
+	#Calculate the direction the player should move.
 	var direction = (forward_dir * input_dir.y + right_dir * input_dir.x).normalized()
 
 	if direction:
