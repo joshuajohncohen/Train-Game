25b583c2cc3868dbad7d75b6b4f550b75626b320
izie <238573415+izieeee@users.noreply.github.com>
2026-03-01 18:17:34 -0500

    weeeee

--- DRUGS.gd
+++ DRUGS.gd
@@ -1,11 +1,43 @@
 extends Node
 
+@onready var player = get_node("/root/node_3d/Player_Spawn/Player")
+@onready var shader_rect = get_node("root/Node3D/Player_Spawn/Player/ColorRect")
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+var normal_speed: float = 5.0
+var effect_duration: float = 3.0
 
+func anti_sanityanters():
+	print("meow!")
+	var the_random = randi_range(0, 2)
+	
+	if the_random == 0:
+		print("DRUG TIME")
+		if shader_rect:
+			var mat = shader_rect.material as ShaderMaterial
+			var tween = create_tween()
+			tween.tween_property(mat, "shader_parameter/distortion_strength", 0.05, 0.5)
+			
+			await get_tree().create_timer(effect_duration).timeout
+			
+			var tween_back = create_tween()
+			tween_back.tween_property(mat, "shader_parameter/distortion_strength", 0.0, 1.0)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+	elif the_random == 1:
+		print("FASTER")
+		apply_speed_change(10.0)
+
+	elif the_random == 2:
+		print("SLOWER")
+		apply_speed_change(2.0)
+
+func apply_speed_change(new_speed: float):
+	if player:
+		Global.speed_boost = new_speed
+		await get_tree().create_timer(effect_duration).timeout
+		if is_instance_valid(player):
+			Global.speed_boost = normal_speed
+
+func _on_area_3d_body_entered(body: Node3D) -> void:
+	print(body)
+	if body is CharacterBody3D:
+		anti_sanityanters()
--- move_player.gd
+++ move_player.gd
@@ -1,7 +1,7 @@
 extends CharacterBody3D
 # https://opensourcegamers.com/wp-blog/2024/08/24/how-to-make-a-simple-3d-player-in-godot-4-0/
 const SPEED = 10
-const JUMP_VELOCITY = 7
+const JUMP_VELOCITY = 5
 const SENSITIVITY = .010
 #Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
@@ -39,7 +39,8 @@ func _physics_process(delta):
 
 	#Calculate the direction the player should move.
 	var direction = (forward_dir * input_dir.y + right_dir * input_dir.x).normalized()
-
+	velocity.x = velocity.x * Global.speed_boost
+	velocity.z = velocity.z * Global.speed_boost
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
