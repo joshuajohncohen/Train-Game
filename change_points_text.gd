3877fe55197c807459d7d326b5c93f04ca323bb4
izie <238573415+izieeee@users.noreply.github.com>
2026-02-28 19:29:56 -0500

    nwhw knws

--- change_points_text.gd
+++ change_points_text.gd
@@ -4,6 +4,6 @@ func _ready():
 	# Enable BBCode programmatically if not done in inspector
 	bbcode_enabled = true
 	
-func _process(delta):
+func _process(_delta):
 	# Setting text with bold and color tags
 	text = "Points: " + str(Global.player_score)
--- pointgiver.gd
+++ pointgiver.gd
@@ -1,7 +1,9 @@
-extends Area3D
+extends Node3D
 
-func _on_body_entered(body: Node3D) -> void:
+func _body_entered(body: Node3D) -> void:
 	# 'body' is the object that just entered
+	print_debug("mrow")
+	print("aaaaa")
 	Global.player_score += 1
 	if body.is_in_group("Player"):
 		print("The Player has entered the zone!")
@@ -9,3 +11,8 @@ func _on_body_entered(body: Node3D) -> void:
 	# Or check for a specific name/type
 	if body is CharacterBody3D:
 		print("A character entered: ", body.name)
+
+
+func _on_area_3d_body_entered(body: Node3D) -> void:
+	_body_entered(body)
+	pass # Replace with function body.
