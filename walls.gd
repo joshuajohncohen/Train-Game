632f5e3e9fd33d6c2bbb2bda5c39c35740bdf6b6
izie <238573415+izieeee@users.noreply.github.com>
2026-03-01 19:03:01 -0500

    e

--- walls.gd
+++ walls.gd
@@ -4,4 +4,4 @@ extends Node3D
 @export var target_material: StandardMaterial3D
 
 func _process(_delta: float) -> void:
-	global_position.x = player.global_position.x
+	global_position.x = player.global_position.x - 25
